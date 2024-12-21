58f8ed3b140587fbcaefbbec1900f61cdd7e67e5
Ahmad Rashidh <41289756+ahmadrashidh@users.noreply.github.com>
2024-12-21 04:24:44 +0000

    entities defined

---
+++ docs/design.md
@@ -0,0 +1,15 @@
+# Designing Product Service
+
+## Entities and its attributes
+
+### Category
+- id - Long
+- name - String
+
+### Product
+- id - Long
+- name - String
+- description - String
+- category - Category
+- price - Double
+
---
+++ src/main/java/com/ahmad/product_service/models/Product.java
@@ -0,0 +1,25 @@
+package com.ahmad.product_service.models;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Product extends Base{
+
+    private String name;
+
+    private String description;
+
+    private Long price;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Category category;
+
+    private String image;
+}
